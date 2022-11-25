be84a854f41df727a0cdc9c71b0b80632d0228ab
amna <amnaajward@gmail.com>
2022-11-25 20:26:57 +0530

    delete unwanted files

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { useRef } from "react";
 import { Button } from "react-bootstrap";
 import useDraw from "../hooks/useDraw";
-import useUpdateLogger from "../hooks/useUpdateLogger";
 
 type CanvasProps = {
 	width: number;
@@ -15,20 +14,20 @@ export default function Canvas({ width, height }: CanvasProps) {
 	const [draw] = useDraw();
 
 	const [queries, setQueries] = useState<string[]>([
-		// "wdl-AB-3",
-		// // "pb-AB",
-		// "sa-AB~A-C-4",
+		"wdl-AB-3",
+		"pb-AB",
+		"sa-AB~A-C-4",
 		// "ab-^CAB",
 
 		//triangle 3 length given
-		"wdl-AB-4", //Must to bring the drawing to center of the canvas
-		"trw-ABC-AC-5-BC-4",
+		// "wdl-AB-4", //Must to bring the drawing to center of the canvas
+		// "trw-ABC-AC-5-BC-4",
 
 		//triangle 1 angle & 1 length given
 		// "tra-ABC-^ABC-90-AC-6",
 
 		// "c-C-AC",
-		"px-AB~C",
+		// "px-AB~C",
 	]);
 
 	useEffect(() => {
