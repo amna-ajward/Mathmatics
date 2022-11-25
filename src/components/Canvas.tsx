d38ad16f6cf33b9d26ead90eadb78af06ba4cefd
Aheek Azmy <atheekark@gmail.com>
2022-11-25 20:10:25 +0530

    added next and previous steps

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -14,22 +14,24 @@ export default function Canvas({ width, height }: CanvasProps) {
 	const canvasCtxRef = React.useRef<CanvasRenderingContext2D | null>(null);
 	const [draw] = useDraw();
 
+
 	const [queries, setQueries] = useState<string[]>([
-		// "wdl-AB-3",
-		// // "pb-AB",
-		// "sa-AB~A-C-4",
-		// "ab-^CAB",
+		"wdl-AB-3",
+		"pb-AB",
+		"sa-AB~A-C-4",
+		// // "ab-^CAB",
 
 		//triangle 3 length given
-		"wdl-AB-4", //Must to bring the drawing to center of the canvas
-		"trw-ABC-AC-5-BC-4",
+		// "wdl-AB-4", //Must to bring the drawing to center of the canvas
+		// "trw-ABC-AC-5-BC-4",
 
 		//triangle 1 angle & 1 length given
 		// "tra-ABC-^ABC-90-AC-6",
 
 		// "c-C-AC",
-		"px-AB~C",
+		// "px-AB~C",
 	]);
+	const [currentStep, setCurrentStep] = useState(queries.length);
 
 	useEffect(() => {
 		if (canvasRef.current) {
@@ -38,20 +40,41 @@ export default function Canvas({ width, height }: CanvasProps) {
 		}
 	}, []);
 
+	useEffect(() => {
+		draw({ queries: queries.slice(0, currentStep), canvasDimension: canvasDimension })
+	}, [currentStep]);
+
 	const canvasDimension = { w: width, h: height };
+
+	const onDraw = () => draw({
+		queries: queries,
+		canvasDimension: canvasDimension,
+	})
+
 	return (
-		<>
-			<Button
-				onClick={() =>
-					draw({
-						queries: queries,
-						canvasDimension: canvasDimension,
-					})
-				}
-			>
-				Canvas Run
-			</Button>
+		<div >
 			<canvas ref={canvasRef} width={width} height={height}></canvas>
-		</>
+			<div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+				<Button
+					onClick={onDraw}
+				>
+					Canvas Run
+				</Button>
+				<Button
+					disabled={currentStep == 1}
+					onClick={() => setCurrentStep(currentStep - 1)}
+				>
+					Previous step
+				</Button>
+				<Button
+					disabled={currentStep == queries.length}
+					onClick={() => setCurrentStep(currentStep + 1)}
+				>
+					Next step
+				</Button>
+
+			</div>
+
+		</div>
 	);
 }
