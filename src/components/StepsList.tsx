8b1ea0dc884977bf4e5be3d035b9e4198a076ff9
amna <amnaajward@gmail.com>
2022-12-02 12:43:01 +0530

    Remove comments

--- src/components/StepsList.tsx
+++ src/components/StepsList.tsx
@@ -2,14 +2,15 @@ import React from "react";
 
 export const StepsList = ({ steps, currentStep, onStepClick }) => {
 	return (
-		<div className="step-list">
+		<div className="step-list ">
 			{steps.map((step, i) => (
 				<div
 					key={i}
 					onClick={() => onStepClick(i + 1)}
-					className={`card ${currentStep - 1 == i ? "active" : ""}`}
+					className={`card ${currentStep - 1 == i ? "active" : ""} `}
+					style={{ width: "100%" }}
 				>
-					<div className="container">
+					<div className="container d-flex gap-2">
 						<h6>Step {i + 1}</h6>
 						<p>{step}</p>
 					</div>
